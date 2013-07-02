b0b5c816b4731583abe423626c39807cdd7dfc92
William Hughes <whlabratz@gmail.com>
2013-07-02 17:23:38 +1200

    ensure sources end in /

--- manifests/backup.pp
+++ manifests/backup.pp
@@ -1,8 +1,14 @@
 define simplebackup::backup($source=$name, $listfile="/var/backuplist") {
 
+	if $source =~ /.*//$/ {
+		$t  = $source
+	} else {
+		$s = "${source}/"
+	}
+
 	concat::fragment{"backup_${source}":
       target => $listfile,
-      content => "${source}\n",
+      content => "${t}\n",
       order   => 01,
    }
 
