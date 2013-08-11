11cfc8bf8377f17afa2c35193f529cedc97d595c
William Hughes <whlabratz@gmail.com>
2013-08-12 11:26:00 +1200

    allow for customizable backup runtimes

--- manifests/backupsystem.pp
+++ manifests/backupsystem.pp
@@ -1,4 +1,4 @@
-define simplebackup::backupsystem($server=$name, $serverpath, $serveruser="root", $listfile="/var/backuplist", $key="/root/.ssh/id_rsa") {
+define simplebackup::backupsystem($server=$name, $serverpath, $serveruser="root", $listfile="/var/backuplist", $key="/root/.ssh/id_rsa", $cron_hour=ip_to_cron(1,24),$cron_minute=0) {
 	
 	file { "/bin/backup.sh":
 		owner => "root",
@@ -11,8 +11,8 @@ define simplebackup::backupsystem($server=$name, $serverpath, $serveruser="root"
 	cron { "backup":
 		command => "/bin/backup.sh",
 		user 	=> "root",
-		hour    => ip_to_cron(1,24),
-		minute	=> 0,
+		hour    => $cron_hour,
+		minute	=> $cron_minute,
 	}
 
 	concat { $listfile:
